fbedac389744b6538731a4ff29ba7ceabc9b69b7
unknown <drakem@2S21B42.pdx.odshp.com>
2015-12-30 10:49:25 -0800

    dont quote the columns

--- 835asCSV.py
+++ 835asCSV.py
@@ -18,7 +18,7 @@ import requests
 
 # 835 status codes published online by WPC
 myURL = "www.wpc-edi.com/reference/codelists/healthcare/claim-adjustment-reason-codes/"
-CSV_OUT = "'{0}', '{1}'"
+CSV_OUT = "{0}, {1}"
 
 page = urllib.request.urlopen("http://"+myURL)
 soup = BeautifulSoup(page, "html.parser")
@@ -28,12 +28,12 @@ rows = soup.find_all('tr',{'class':'prod_set current'})
 # put into a python list
 data = [[td.findChildren(text=True) for td in tr.findAll("td")] for tr in rows]
 
-# Output as CSV. Make sure to double-quote single quotes
+# Output as CSV
 print("//automatically from www.wpc-edi.com Claim Adjustment Reason Codes ASC X12 External Code Source 139 ")
 
 for item in data:
   #truncate before any "Notes"
   if item[1][0].find('Note:') > 0 :
-    print(CSV_OUT.format(item[0][0], item[1][0].replace("'", "''")[:item[1][0].find('Note:')]))
+    print(CSV_OUT.format(item[0][0], item[1][0][:item[1][0].find('Note:')]))
   else:
-    print(CSV_OUT.format(item[0][0], item[1][0].replace("'", "''")))
+    print(CSV_OUT.format(item[0][0], item[1][0]))
